3cc241170b828be6c2fa8143023c504180f04653
pratiktekanar <pratiktekanar5044@gmail.com>
2025-08-01 10:10:26 +0530

    Add basic Login and Register pages

---
+++ src/main/java/com/e_commerce/e_commerce/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.e_commerce.e_commerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+}
